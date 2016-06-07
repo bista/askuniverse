f52c99ce82601da4b6e8a2ae6c3018a180447b9e
bista <bishnubista@outlook.com>
2016-06-07 13:07:14 -0500

    Question part works

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,12 @@
 class AnswersController < ApplicationController
+
+	def create
+		Answer.create(answer_params)
+	end
+
+	private
+
+	def answer_params
+		params.require(:answer).permit(:email, :body)
+	end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,9 @@
 class QuestionsController < ApplicationController
 # skip_before_filter  :verify_authenticity_token
+	def show
+		@question = Question.find(params[:id])
+	end
+
 	def create
 		Question.create(question_params)
 		redirect_to root_path
@@ -7,6 +11,6 @@ class QuestionsController < ApplicationController
 		
 	private 
 		def question_params
-			require(:question).permit(:email, :body)
+			params.require(:question).permit(:email, :body)
 		end
 end
